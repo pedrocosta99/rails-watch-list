d4179d424d66ebcd1de943431522cfa08d65d180
Pedro Henrique Costa <pedrocosta99@gmail.com>
2022-05-27 17:35:09 -0300

    g scaffold movies, g model  list and bookmarks

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # get 'bookmarks/create', as: :bookmark_new
+  root to: 'lists#index'
   resources :lists, except: %i[update] do
     resources :bookmarks, only: %i[new create index]
   end
